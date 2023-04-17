87743fbb2c9dff88863394ff7a114aa048794aa4
dyalnpahm <117327438+dyalnpahm@users.noreply.github.com>
2023-04-17 19:03:36 +1000

    fixed syntax of engineers in engineer.test.js

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,6 +1,6 @@
 const Engineer = require('..lib/Engineer');
 
-test ('creates an engineer card', () => {
+test ('creates engineer card', () => {
     const engineer = new Engineer('Jason', 20, 'Jason.yung@gmail.com', 'jasonyung03');
 
     expect (engineer.github).toBe(expect.any(string));
@@ -12,8 +12,8 @@ test('gets the engineers github', () =>{
     expect (engineer.getGithub()).toBe(expect.stringContaining(engineer.github.toString()));
 });
 
-test('gets the role of engineer', ()=>{
-    const engineer=new engineer ('Jason', 20, 'Jason.yung@gmail.com', 'jasonyung03');
+test('gets the role of the engineer', ()=>{
+    const engineer= new Engineer ('Jason', 20, 'Jason.yung@gmail.com', 'jasonyung03');
 
     expect(engineer.getRole()).toBe('Engineer');
 });
