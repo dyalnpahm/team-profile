f044761e40b98b75327708a14da8779351920bf2
dyalnpahm <117327438+dyalnpahm@users.noreply.github.com>
2023-04-17 20:30:18 +1000

    added employee prompts to index.js

--- index.js
+++ index.js
@@ -3,119 +3,76 @@ const generateTeam = require ('./src/TemplateHTML.js');
 const fs =require('fs');
 const inquirer = require ('inquirer');
 
-const Manager = require('./lib/Manager');
+const Manager = require('./lib/manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern'); 
 
 const newTeam = [];
 
-const questions = async () => {
-    const answers = await inquirer
-    return inquirer.prompt([
-        {
-        type: 'input',
-        message: 'What is your name?',
-        name: 'name',
+const teamManager  = () => {
+  return inquirer.prompt([
 
-    },
-    {
-        type: 'input',
-        message: 'What is your ID',
-        name: 'id',
+  {
+    type: 'input',
+    name: 'name',
+    message: "Enter team manager's name",
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+
+      } else {
+        console.log ("Please enter the manager's name");
+        return false;
+      }
+    }
+  },
+{
+  type: "input",
+  name: "id",
+  message: "Enter team manager's iD.",
+  validate: IDInput => {
+    if (isNaN(IDInput)){
+      console.log ("Please enter the manager's ID");
+      return false;
+
+    } else {
+      return true;
+    }
+  }
 
 },
 {
-    type:'input',
-    message: 'What is your email',
-    name: 'email'
-
-},
-])
-
-if (answers.role === "Manager") {
-    const managerAns = await inquirer
-      .prompt([
-        {
-          type: "input",
-          message: "What is your office number",
-          name: "officeNumber",
-        },
-      ])
-      const newManager = new Manager(
-        answers.name,
-        answers.id,
-        answers.email,
-        managerAns.officeNumber
-      );
-      newTeam.push(newTeam);
-      
-  
-  } else if (answers.role === "Engineer") {
-    const githubAns = await inquirer
-      .prompt([
-        {
-          type: "input",
-          message: "What is your GitHub user?",
-          name: "github",
-        }
-      ])
-        const newEngineer = new Engineer(
-          answers.name,
-          answers.id,
-          answers.email,
-          githubAns.github
-        );
-        newTeam.push(newTeam);
-      
-
-  } else if (answers.role === "Intern") {
-    const internAns = await inquirer
-      .prompt([
-        {
-          type: "input",
-          message: "What university did you attend?",
-          name: "school",
-        },
-      ])
-      
-      const newIntern = new Intern(
-        answers.name,
-        answers.id,
-        answers.email,
-        internAns.school
-      );
-      newTeam.push(newTeam);          
-  } 
-
-}; 
-
-async function promptQuestions() {
-await questions()
-
-
-const addMemberAns = await inquirer
-.prompt([
-  {
-    name:'addMember',
-    type: 'list',
-    choices: ['Add a new member', 'Create team'],
-    message: "What would you like to do next?"
+  type: 'input',
+  name: 'email',
+  message: "Enterr the manager's email.",
+  validate: emailInput => {
+    if (emailInput.includes('@') ){
+      return true;
+    } else {
+      console.log('Please enter the employees email address!');
+      return false;
+      }
   }
+},
+{
+  type: 'input',
+  name: 'officeNumber',
+  message: "Enter the manager's office number",
+  validate: officeInput => {
+      if  (isNaN(officeInput)) {
+          console.log ('Please enter an office number!')
+          return false; 
+      } else {
+          return true;
+      }
+  }
+}
 ])
+.then(managerInput =>{ 
+  const {name, id, email, officeNumber}  = managerInput;
+  const manager = new Manager (name, id, email, officeNumber);
+  newTeam.push(manager);
+  console.log(manager);
 
-if (addMemberAns.addMember === 'Add a new member') {
-  return promptQuestions()
-}
-return createTeam();
-}  
-
-promptQuestions();
-
-function createTeam () {
-console.log("new guy", newTeam)
-fs.writeFileSync(
-"./output/index.html",
-generateTeam(newTeam),
-"utf-8"
-);
-}
+})
+};
