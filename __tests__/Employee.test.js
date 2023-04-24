ec7fc4d050c3de0b050ce4542a9c8cc15fb84c01
dyalnpahm <117327438+dyalnpahm@users.noreply.github.com>
2023-04-24 18:49:49 +1000

    fixed syntax error so manager and employee tests pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,27 +3,27 @@ const Employee = require('../lib/employee');
 test('creates new employee', () => {
     const employee = new Employee('Jason', 20, 'Jason.yung@gmail.com');
 
-    expect(employee.name).toBe(expect.any(String));
-    expect(employee.id).toBe(expect.any(Number));
-    expect(employee.email).toBe(expect.any(String));
+    expect(employee.name).toStrictEqual(expect.any(String));
+    expect(employee.id).toStrictEqual(expect.any(Number));
+    expect(employee.email).toStrictEqual(expect.any(String));
 });
 
 test('gets employee name', () => {
     const employee = new Employee('Jason', 20, 'Jason.yung@gmail.com');
 
-    expect(employee.getName()).toBe(expect.any(String));
+    expect(employee.getName()).toStrictEqual(expect.any(String));
 });
 
 test('gets employee ID', () => {
     const employee = new Employee('Jason', 20, 'Jason.yung@gmail.com');
 
-    expect(employee.getId()).toBe(expect.any(Number));
+    expect(employee.getId()).toStrictEqual(expect.any(Number));
 });
 
 test('gets employee email', () => {
     const employee = new Employee('Jason', 20, 'Jason.yung@gmail.com');
 
-    expect(employee.getEmail()).toBe(expect.stringContaining(employee.email.toString()));
+    expect(employee.getEmail()).toStrictEqual(expect.stringContaining(employee.email.toString()));
 });
 
 test('gets role of employee', () => {
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,12 +1,12 @@
 const Manager = require('../lib/manager');
 
 test ('creates manager card', () => {
-    const manager = new Manager ('Jason', 20, 'Jason.yung@gmail.com', '2');
-    expect (manager.officeNumber).toBe(expect.any(Number));
+    const manager = new Manager ('Jason', 20, 'Jason.yung@gmail.com', 2);
+    expect (manager.officeNumber).toStrictEqual(expect.any(Number));
 
 });
 
 test('gets role of manager',()=>{
-    const manager = new Manager ('Jason', 20, 'Jason.yung@gmail.com', '2');
-    expect ( manager.getRole()).toBe("Manager");
+    const manager = new Manager ('Jason', 20, 'Jason.yung@gmail.com');
+    expect ( manager.getRole()).toStrictEqual("Manager");
 });
