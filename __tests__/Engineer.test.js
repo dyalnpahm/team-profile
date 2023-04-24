ce2f1b82a3f7589fcc899995e3d23ffc5c5d488c
dyalnpahm <117327438+dyalnpahm@users.noreply.github.com>
2023-04-24 18:57:41 +1000

    fixed tests so all pass

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,15 +1,15 @@
-const Engineer = require('..lib/Engineer');
+const Engineer = require('../lib/Engineer');
 
 test ('creates engineer card', () => {
     const engineer = new Engineer('Jason', 20, 'Jason.yung@gmail.com', 'jasonyung03');
 
-    expect (engineer.github).toBe(expect.any(string));
+    expect (engineer.github).toStrictEqual(expect.any(String));
 });
 
 test('gets the engineers github', () =>{
     const engineer = new Engineer ('Jason', 20, 'Jason.yung@gmail.com', 'jasonyung03');
 
-    expect (engineer.getGithub()).toBe(expect.stringContaining(engineer.github.toString()));
+    expect (engineer.getGithub()).toStrictEqual(expect.stringContaining(engineer.github.toString()));
 });
 
 test('gets the role of the engineer', ()=>{
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -2,12 +2,12 @@ const Intern=require('../lib/Intern');
 
 test ('creates an intern card', ()=> {
     const intern = new Intern ('Jason', 20, 'Jason.yung@gmail.com', 'unsw');
-    expect(intern.school).toBe(expect.any(string));
+    expect(intern.school).toStrictEqual(expect.any(String));
 }); 
 
 test('gets intern school',()=>{
     const intern = new Intern ('Jason', 20, 'Jason.yung@gmail.com', 'unsw');
-    expect(intern.getSchool()).toBe(expect.stringContaining(intern.school.toString()));
+    expect(intern.getSchool()).toStrictEqual(expect.stringContaining(intern.school.toString()));
 })
 
 test('gets role of the intern',()=>{
